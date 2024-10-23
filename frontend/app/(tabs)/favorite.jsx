627a7a351896b52618fcc518bbeff10d5a0ef591
Saleh <39927806+habtor@users.noreply.github.com>
2024-10-23 11:03:58 +0200

    fixe fav logic

--- frontend/app/(tabs)/favorite.jsx
+++ frontend/app/(tabs)/favorite.jsx
@@ -8,42 +8,49 @@ import FavCard from "../../components/favCard";
 
 const Favourite = () => {
   const { fav, addToFav, removeFromFav } = useFav();
+  console.log(fav);
 
   return (
     <GestureHandlerRootView>
-      <View className="">
-        <Text className="text-3xl text-center font-bold mt-8">Favourite</Text>
-        <FlatList
-          className="mt-8"
-          showsHorizontalScrollIndicator={false}
-          data={fav}
-          keyExtractor={(item) => item._id}
-          renderItem={({ item }) => (
-            <Swipeable
-              renderRightActions={() => (
-                <TouchableOpacity
-                  className="w-24 h-full items-center justify-center"
-                  onPress={() => removeFromFav(item)}
-                >
-                  <Ionicons name="trash-outline" size={40} color="red" />
-                </TouchableOpacity>
-              )}
-            >
-              <FavCard
-                name={item.name}
-                addon={item.addon}
-                image={item.image}
-                description={item.description}
-                price={item.price}
-                size={item.size}
-                rating={item.rating}
-                numReviews={item.numReviews}
-                count={item.count}
-              />
-            </Swipeable>
-          )}
-        />
-      </View>
+      {!fav || fav.length === 0 ? (
+        <Text className="text-3xl text-black text-center font-bold mt-24">
+          You don't have favourites
+        </Text>
+      ) : (
+        <View className="">
+          <Text className="text-3xl text-center font-bold mt-8">Favourite</Text>
+          <FlatList
+            className="mt-8"
+            showsHorizontalScrollIndicator={false}
+            data={fav}
+            keyExtractor={(item) => item._id}
+            renderItem={({ item }) => (
+              <Swipeable
+                renderRightActions={() => (
+                  <TouchableOpacity
+                    className="w-24 h-full items-center justify-center"
+                    onPress={() => removeFromFav(item)}
+                  >
+                    <Ionicons name="trash-outline" size={40} color="red" />
+                  </TouchableOpacity>
+                )}
+              >
+                <FavCard
+                  name={item.name}
+                  addon={item.addon}
+                  image={item.image}
+                  description={item.description}
+                  price={item.price}
+                  size={item.size}
+                  rating={item.rating}
+                  numReviews={item.numReviews}
+                  count={item.count}
+                />
+              </Swipeable>
+            )}
+          />
+        </View>
+      )}
     </GestureHandlerRootView>
   );
 };
